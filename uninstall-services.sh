0187a0cd522791ab26fa536ffd9fd114e183d1ee
Mohammed Ismail <mohammedismail72425@gmail.com>
2024-02-13 23:12:58 +0530

    Added uninstall.sh

---
+++ uninstall-services.sh
@@ -0,0 +1,11 @@
+kubectl delete -f kubernetes-services/adservice-service.yaml -n microservices
+kubectl delete -f kubernetes-services/cartservice-service.yaml -n microservices
+kubectl delete -f kubernetes-services/checkoutservice-service.yaml -n microservices
+kubectl delete -f kubernetes-services/currencyservice-service.yaml -n microservices
+kubectl delete -f kubernetes-services/emailservice-service.yaml -n microservices
+kubectl delete -f kubernetes-services/frontend-service.yaml -n microservices
+kubectl delete -f kubernetes-services/paymentservice-service.yaml -n microservices
+kubectl delete -f kubernetes-services/productcatalogservice-service.yaml -n microservices
+kubectl delete -f kubernetes-services/recommendationservice-service.yaml -n microservices
+kubectl delete -f kubernetes-services/redis-cart-service.yaml -n microservices
+kubectl delete -f kubernetes-services/shippingservice-service.yaml -n microservices
